294031ffc8b4459e910aec88ed9552abacb03246
Thiago-Portella <thiagoportellaferreira@gmail.com>
2023-11-25 22:16:50 -0300

    código modificado.
    tela recebe um novo formato de exibição. Ainda não fuciona como deveria. Ao clicar em prosseguir, a tela de seleção não aparece.

--- renomeador de arquivos e pastas/Main.py
+++ renomeador de arquivos e pastas/Main.py
@@ -9,33 +9,51 @@ class Livro:
         self.serie = serie
         self.titulo = titulo
 
-def obter_caminho():
-    app = wx.App(False)
-    dialog = wx.FileDialog(None, "Selecione o arquivo ou pasta", wildcard="All Files (*.*)|*.*", style=wx.FD_OPEN)
-    if dialog.ShowModal() == wx.ID_OK:
-        caminho = dialog.GetPath()
-    else:
-        caminho = ""
-    dialog.Destroy()
-    return caminho
-
-class RenomeadorApp(wx.Frame):
+class EscolhaTipo(wx.Frame):
     def __init__(self):
-        super().__init__(None, title='Renomeador de Arquivos')
-
-        self.lista_de_informacoes = []
+        super().__init__(None, title='Escolha o Tipo', size=(300, 150))
 
         self.initUI()
-        self.Show()
 
     def initUI(self):
         panel = wx.Panel(self)
         sizer = wx.BoxSizer(wx.VERTICAL)
 
-        # Campos de entrada
+        label_tipo = wx.StaticText(panel, label="Escolha o Tipo:")
+        choices = ["Arquivo", "Pasta"]
+        self.dropdown_tipo = wx.Choice(panel, choices=choices)
+        btn_prosseguir = wx.Button(panel, label="Prosseguir")
+        btn_prosseguir.Bind(wx.EVT_BUTTON, self.prosseguir)
+
+        sizer.Add(label_tipo, 0, wx.ALL, 10)
+        sizer.Add(self.dropdown_tipo, 0, wx.ALL | wx.EXPAND, 10)
+        sizer.Add(btn_prosseguir, 0, wx.ALL, 10)
+
+        panel.SetSizer(sizer)
+
+    def prosseguir(self, event):
+        tipo_selecionado = self.dropdown_tipo.GetStringSelection()
+        self.Hide()
+        TelaPrincipal(tipo_selecionado).Show()
+
+class TelaPrincipal(wx.Frame):
+    def __init__(self, tipo):
+        super().__init__(None, title='Renomeador de Arquivos', size=(400, 300))
+
+        self.tipo = tipo
+        self.caminho = ""
+        self.lista_de_informacoes = []
+
+        self.initUI()
+
+    def initUI(self):
+        panel = wx.Panel(self)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
         self.label_caminho = wx.StaticText(panel, label="Caminho:")
         self.entry_caminho = wx.TextCtrl(panel, style=wx.TE_READONLY)
-        self.entry_caminho.SetHint("Insira o caminho ou clique em 'Procurar'")
+        self.btn_procurar = wx.Button(panel, label="Procurar")
+        self.btn_procurar.Bind(wx.EVT_BUTTON, self.procurar_caminho)
 
         self.label_autor = wx.StaticText(panel, label="Autor:")
         self.entry_autor = wx.TextCtrl(panel)
@@ -46,20 +64,15 @@ class RenomeadorApp(wx.Frame):
         self.label_titulo = wx.StaticText(panel, label="Título:")
         self.entry_titulo = wx.TextCtrl(panel)
 
-        self.label_tipo = wx.StaticText(panel, label="Tipo:")
-        self.dropdown_tipo = wx.Choice(panel, choices=["Arquivo", "Pasta"])
+        btn_renomear = wx.Button(panel, label="Renomear")
+        btn_renomear.Bind(wx.EVT_BUTTON, self.renomear)
+        btn_adicionar_outro = wx.Button(panel, label="Renomear e Adicionar Outro")
+        btn_adicionar_outro.Bind(wx.EVT_BUTTON, self.renomear_e_adicionar)
+        btn_cancelar = wx.Button(panel, label="Cancelar")
+        btn_cancelar.Bind(wx.EVT_BUTTON, self.cancelar)
+        btn_voltar = wx.Button(panel, label="Voltar")
+        btn_voltar.Bind(wx.EVT_BUTTON, self.voltar)
 
-        # Botões
-        self.btn_adicionar = wx.Button(panel, label="Adicionar Livro")
-        self.btn_adicionar.Bind(wx.EVT_BUTTON, self.adicionar_livro)
-
-        self.btn_procurar = wx.Button(panel, label="Procurar")
-        self.btn_procurar.Bind(wx.EVT_BUTTON, self.procurar_caminho)
-
-        self.btn_renomear = wx.Button(panel, label="Renomear Arquivos")
-        self.btn_renomear.Bind(wx.EVT_BUTTON, self.renomear_arquivos)
-
-        # Adiciona widgets ao sizer
         sizer.Add(self.label_caminho, 0, wx.ALL, 5)
         sizer.Add(self.entry_caminho, 0, wx.EXPAND | wx.ALL, 5)
         sizer.Add(self.btn_procurar, 0, wx.ALL, 5)
@@ -69,57 +82,53 @@ class RenomeadorApp(wx.Frame):
         sizer.Add(self.entry_serie, 0, wx.EXPAND | wx.ALL, 5)
         sizer.Add(self.label_titulo, 0, wx.ALL, 5)
         sizer.Add(self.entry_titulo, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.label_tipo, 0, wx.ALL, 5)
-        sizer.Add(self.dropdown_tipo, 0, wx.EXPAND | wx.ALL, 5)
-        sizer.Add(self.btn_adicionar, 0, wx.ALL, 5)
-        sizer.Add(self.btn_renomear, 0, wx.ALL, 5)
+        sizer.Add(btn_renomear, 0, wx.ALL, 5)
+        sizer.Add(btn_adicionar_outro, 0, wx.ALL, 5)
+        sizer.Add(btn_cancelar, 0, wx.ALL, 5)
+        sizer.Add(btn_voltar, 0, wx.ALL, 5)
 
         panel.SetSizer(sizer)
 
-    def adicionar_livro(self, event):
-        tipo = self.dropdown_tipo.GetStringSelection()
-        caminho = self.entry_caminho.GetValue()
+    def procurar_caminho(self, event):
+        style = wx.FD_OPEN if self.tipo == "Arquivo" else wx.DD_DIR_MUST_EXIST
+        dialog = wx.FileDialog(None, "Selecione o arquivo ou pasta", style=style)
+        if dialog.ShowModal() == wx.ID_OK:
+            self.caminho = dialog.GetPath()
+            self.entry_caminho.SetValue(self.caminho)
+        dialog.Destroy()
+
+    def renomear(self, event):
         autor = self.entry_autor.GetValue()
         serie = self.entry_serie.GetValue()
         titulo = self.entry_titulo.GetValue()
 
-        if caminho and autor and serie and titulo:
-            livro = Livro(tipo, caminho, autor, serie, titulo)
-            self.lista_de_informacoes.append(livro)
-            self.limpar_campos()
-        else:
-            print("Por favor, preencha todos os campos.")
-
-    def procurar_caminho(self, event):
-        caminho = obter_caminho()
-        if caminho:
-            self.entry_caminho.SetValue(caminho)
-
-    def limpar_campos(self):
-        self.entry_caminho.Clear()
-        self.entry_autor.Clear()
-        self.entry_serie.Clear()
-        self.entry_titulo.Clear()
-
-    def renomear_arquivos(self, event):
-        for livro in self.lista_de_informacoes:
-            tipo = livro.tipo
-            caminho = livro.caminho
-            autor = livro.autor.title()
-            serie = livro.serie.title()
-            titulo = livro.titulo.title()
-
+        if self.caminho and autor and serie and titulo:
             novo_nome = f"{autor} - ({serie}) - {titulo}"
 
-            if tipo.lower() == "pasta":
-                os.rename(caminho, os.path.join(os.path.dirname(caminho), novo_nome))
-            elif tipo.lower() == "arquivo":
-                extensao = os.path.splitext(caminho)[1]
-                os.rename(caminho, os.path.join(os.path.dirname(caminho), novo_nome + extensao))
+            if self.tipo.lower() == "pasta":
+                os.rename(self.caminho, os.path.join(os.path.dirname(self.caminho), novo_nome))
+            elif self.tipo.lower() == "arquivo":
+                extensao = os.path.splitext(self.caminho)[1]
+                os.rename(self.caminho, os.path.join(os.path.dirname(self.caminho), novo_nome + extensao))
             else:
                 print("Tipo inválido. Por favor, escolha 'pasta' ou 'arquivo'.")
 
+            self.Destroy()  # Fecha a janela após renomear
+        else:
+            print("Por favor, preencha todos os campos.")
+
+    def renomear_e_adicionar(self, event):
+        self.renomear(event)
+        EscolhaTipo().Show()  # Inicia o fluxo novamente
+
+    def cancelar(self, event):
+        self.Destroy()
+
+    def voltar(self, event):
+        EscolhaTipo().Show()
+        self.Destroy()
+
 if __name__ == '__main__':
     app = wx.App(False)
-    frame = RenomeadorApp()
+    EscolhaTipo().Show()
     app.MainLoop()
